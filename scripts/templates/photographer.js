8239f68013d521380cd5752754e88600b378266d
Florian Le Cossec <>
2024-03-08 15:31:30 +0100

    init project + fetch data

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,26 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+	const { name, portrait, city, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+	const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+	function getUserCardDOM() {
+		const article = document.createElement( 'article' );
+		const img = document.createElement( 'img' );
+		img.setAttribute('src', picture);
+		const h2 = document.createElement( 'h2' );
+		h2.textContent = name;
+		const h3 = document.createElement( 'h3' );
+		h3.textContent = city;
+		const p = document.createElement( 'p' );
+		p.textContent = tagline;
+		const span = document.createElement( 'span' );
+		span.textContent = `${price}/jour`;
+		article.appendChild(img);
+		article.appendChild(h2);
+		article.appendChild(h3);
+		article.appendChild(p);
+		article.appendChild(span);
+		return (article);
+	}
+	return { name, picture, getUserCardDOM };
 }
